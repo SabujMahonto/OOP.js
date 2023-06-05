21202ecb93fdd5432a9c07e3c9b26a5921a721b3
sabujmahonto <sabujmahonto@gmail.com>
2023-06-05 12:55:08 +0600

    how to declare  a Class done

---
+++ script.js
@@ -0,0 +1,16 @@
+// functional prototype
+//class-bast prototype
+//object-create() function
+
+// Class
+class Student {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+}
+
+const student1 = new Student("Rahim Ahamed", 1990);
+const student2 = new Student("Sabuj Mahaonto", 1994);
+console.log(student1);
+console.log(student2);
