9fc6d1db74279da33ddc9f161ded7109a331cc11
sabujmahonto <sabujmahonto@gmail.com>
2023-06-05 13:01:28 +0600

    Aplay a method

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ class Student {
     this.fullName = fullName;
     this.birthYear = birthYear;
   }
+  calAge() {
+    return 2023 - this.birthYear;
+  }
 }
 
-const student1 = new Student("Rahim Ahamed", 1990);
+const student1 = new Student("Rahim Ahamed", 1992);
 const student2 = new Student("Sabuj Mahaonto", 1994);
-console.log(student1);
-console.log(student2);
+// console.log(student1);
+// console.log(student2);
+console.log(student1.calAge());
+console.log(student2.calAge());
