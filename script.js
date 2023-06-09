ca54e35dc8b76a707c7999508d78e10359ac5294
sabujmahonto <sabujmahonto@gmail.com>
2023-06-09 21:41:55 +0600

    constructor function done

--- script.js
+++ script.js
@@ -19,3 +19,49 @@ const student2 = new Student("Sabuj Mahaonto", 1994);
 // console.log(student2);
 console.log(student1.calAge());
 console.log(student2.calAge());
+
+console.log(Student.prototype);
+console.log(student1.__proto__);
+
+// Class
+//instance
+// OOP 4 principal\
+//  1. Abstraction
+// 2.Encapsulations
+// 3.inheritance
+// 4.polimotisome
+
+const obj = {
+  age: 1990,
+};
+console.log(obj.hasOwnProperty());
+
+class Test {
+  constructor(birthYear) {
+    this.birthYear = this.birthYear;
+  }
+}
+
+const testObj = new Test(1993);
+console.log(testObj.hasOwnProperty());
+console.log(testObj.hasOwnProperty("birthYear"));
+///////////////////////////////////////////////////////////////////////
+// constructor  function
+/////////////////////////////////////////////////////////////////////
+function Person(nickName, birthYear) {
+  this.nickName = nickName;
+  this.birthYear = birthYear;
+}
+
+const sabuj = new Person("sabuj", "1993");
+const nasim = new Person("nasim", 1990);
+// new create a empty object {}
+// function call this keyword {}
+// {} link to prototype
+//function will automatically return;
+
+console.log(sabuj);
+console.log(nasim instanceof Person);
+// class
+
+// object.create()
